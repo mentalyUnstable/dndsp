53aacc57ff1680ad61eaa009ad3e5cdb90148f2f
mentalyUnstable <18crichardson@quinterschools.org>
2017-04-07 15:38:10 +0000

    commit for dndsp

--- src/util/Changelog.java
+++ src/util/Changelog.java
@@ -2,13 +2,14 @@ package dndsp.util;
 
 import java.util.Scanner;
 import java.io.File;
+import java.net.URL;
 
 public class Changelog{
     
     public static void display(){
         try {
             Util.cls();
-            Scanner s = new Scanner(new URL("http://www.puzzlers.org/pub/wordlists/pocket.txt").openStream());
+            Scanner s = new Scanner(new URL("https://raw.githubusercontent.com/mentalyUnstable/dndsp/master/changes.txt").openStream());
             while(s.hasNextLine())
                 System.out.println(s.nextLine());
             
